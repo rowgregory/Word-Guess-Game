aca1a45d98fd7bd3ad18e5fc3ba2e1b86ea75891
row_gregory <rowgregory@gmail.com>
2018-10-16 16:08:55 -0400

    format document

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,25 +6,25 @@ var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 var selectableWords =           // create an array of words
     [
         {
-            word: "sonicthehedgehog", 
+            word: "sonicthehedgehog",
             image: "assets/images/sonic_giphy.gif",
         },
         {
             word: "shadow",
             image: "assets/images/shadow.gif",
-        }, 
+        },
         {
             word: "knuckles",
-            image:"assets/images/knuckles_gif.gif",
-        }, 
+            image: "assets/images/knuckles_gif.gif",
+        },
         {
             word: "amy",
             image: "assets/images/amy_gif.gif",
-        }, 
+        },
         {
             word: "tailsmilesprower",
             image: "assets/images/tails_gif.gif",
-        }, 
+        },
         {
             word: "eggman",
             image: "assets/images/eggman_giphy.gif",
@@ -48,7 +48,7 @@ var selectableWords =           // create an array of words
         {
             word: "metalsonic",
             image: "assets/images/metal_sonic_gif.gif",
-        
+
         },
         {
             word: "supersonic",
@@ -66,28 +66,26 @@ var selectableWords =           // create an array of words
             word: "soniclostworld",
             image: "assets/images/sonicLostWorld.gif",
         },
-      
+
     ];
-    
+
 var wins = 0;                   // how many wins the player has racked up
 var losses = 0;                 // tallies number of lossses
 var guessesLeft = 12;           // guesses the user has left
 
-var currentWord = ""; 
+var currentWord = "";
 var currentImage = "";          // left empty to store word
 var videoGameLetters = [];      // empty array to store letters of currentWord
 var numBlanks = 0;              // holds the number of "blanks"(where letters will pop up)
 var wrongLtrs = [];             // empty array to store incorrect guesses
 var answer = [];                // empty array to store users answer
-var letterClicked='';
+var letterClicked = '';
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+})
 
-
-
-for (i=0; i < letters.length; i++) {
+for (i = 0; i < letters.length; i++) {
 
     var letterBtn = $("<button>");
 
@@ -95,12 +93,11 @@ for (i=0; i < letters.length; i++) {
     letterBtn.attr("data-letter", letters[i]);
     letterBtn.text(letters[i]);
     $("#buttons").append(letterBtn);
-    };
+};
 
-    $(".letter-button").on("click", function() {
-        letterClicked = $(event.target).text().toLowerCase();
-       letterButtonClicked(letterClicked);
-       console.log(letterClicked);
+$(".letter-button").on("click", function () {
+    letterClicked = $(event.target).text().toLowerCase();
+    letterButtonClicked(letterClicked);
 
     var fridgeMagnet = $("<div>");
 
@@ -109,153 +106,109 @@ for (i=0; i < letters.length; i++) {
     $("answer").append(fridgeMagnet);
 });
 
-
-
-
-
 // DOM manipulation 
 // =============================================
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById("underscore").innerHTML = answer.join(" ");
-    document.getElementById("remaining").innerHTML ="Guesses remaing: " + guessesLeft;
-    document.getElementById("wins").innerHTML = "Wins: " + " "
-     + wins;
+    document.getElementById("remaining").innerHTML = "Guesses remaing: " + guessesLeft;
+    document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
     document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
     document.getElementById("guessed").innerHTML = "Choosen letters: " + " " + wrongLtrs;
-    
-    
 }
 
-
-                    
 function beginGame() {
-    
-    
+
     $("#reset").hide();
     // === uses Math floor to select word at random
-    
     var accessedIndex = Math.floor(Math.random() * selectableWords.length);
     currentWord = selectableWords[accessedIndex].word;
     currentImage = selectableWords[accessedIndex].image;
 
     // === divide (split) randomWord into an individual letters and store it into videoGameletters
     videoGameLetters = currentWord.split("");
-        console.log(videoGameLetters);
+    console.log(videoGameLetters);
 
     // === obtain number of letters in randomWord and store it into numBlanks
-     numBlanks = videoGameLetters.length;
-        console.log(numBlanks);
+    numBlanks = videoGameLetters.length;
 
-            guessesLeft = 12;
-            wrongLtrs = [];
-            answer = [];
+    guessesLeft = 12;
+    wrongLtrs = [];
+    answer = [];
 
-        // === replace (push) the correct amount of letters in the randomWord with underscores 
-        for (i=0; i < numBlanks; i++) {
-            answer.push('_');
-            console.log(answer);
-        }  
-
-        
-        
-        
-    
+    // === replace (push) the correct amount of letters in the randomWord with underscores 
+    for (i = 0; i < numBlanks; i++) {
+        answer.push('_');
     }
-
+}
 
 function letterButtonClicked(x) {
 
     // === determine if key choosen was in the alphabet
-    
-        for (var i = 0; i < videoGameLetters.length; i++) { 		//scans through every letter in the array
-            if (x === videoGameLetters[i]) {  				//if the letter that was clicked equals the letters of the random word
-                answer[i] = x;	
-                document.getElementById("ring").play();				//then replace the blank space with the clicked letter
-            }
+    for (var i = 0; i < videoGameLetters.length; i++) { 		//scans through every letter in the array
+        if (x === videoGameLetters[i]) {  				//if the letter that was clicked equals the letters of the random word
+            answer[i] = x;
+            document.getElementById("ring").play();				//then replace the blank space with the clicked letter
         }
-        $('#underscore').text(answer.join(' '));		//adds answer from letter arrary and displays it
-    
+    }
+    $('#underscore').text(answer.join(' '));		//adds answer from letter arrary and displays it
 
-    
-        if (!(videoGameLetters.indexOf(x) > -1)) {			//if the word does not have the letter 
-            	
-            document.getElementById("killed").play();
-            wrongLtrs.push(x);
-            guessesLeft--;
+    if (!(videoGameLetters.indexOf(x) > -1)) {			//if the word does not have the letter 
 
-            $(this).remove('x').css('color', 'green').unbind('click');
-            						// lives decrement by 1.
-            					
-    
-        }
-        rounds();
-        console.log("This is the answer " + answer);
+        document.getElementById("killed").play();
+        wrongLtrs.push(x);
+        guessesLeft--;
+    }
+    rounds();
+    console.log("This is the answer " + answer);
 }
 
 function rounds() {
 
-    
-    
     document.getElementById("mad").style.visibility = "hidden";
     document.getElementById("winImage").style.visibility = "hidden";
     document.getElementById("remaining").innerHTML = "Guesses remaining: " + " " + guessesLeft;
     document.getElementById("underscore").innerHTML = answer.join(" ");
     document.getElementById("guessed").innerHTML = "Already used: " + " " + wrongLtrs.join(" ");
-    
 
-    if(videoGameLetters.toString() == answer.toString()) {
+    if (videoGameLetters.toString() == answer.toString()) {
         wins++;
-        
-            
+
         //update wins
-        document.getElementById("winImage").src=currentImage
+        document.getElementById("winImage").src = currentImage
         document.getElementById("cash").play();
         document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
         document.getElementById("winImage").style.visibility = "visible";
         alert("You won");
         $("#reset").show();
-        $('#myModal').modal('show');
-        // beginGame();   
-        
+        $('#myModal').modal('show');  
+
     } else if (guessesLeft == 0) {
-            losses++;
-            document.getElementById("shield").play();
-            document.getElementById("mad").style.visibility = "visible";
-            alert("you lose!");
-            $("#reset").show();
-            $('#myModal').modal('show');
-            // beginGame();
-       
+        losses++;
+        document.getElementById("shield").play();
+        document.getElementById("mad").style.visibility = "visible";
+        alert("you lose!");
+        $("#reset").show();
+        $('#myModal').modal('show');
 
         // Update the losses in the HTML document
         document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
-        
-        
-        // beginGame();
         document.getElementById("guessed").innerHTML = "Choosen letters:" + " " + wrongLtrs.join(" ");
-    }  
+    }
 }
-	
-$('#reset').on('click',function(){
-        
+
+$('#reset').on('click', function () {
+
     beginGame();
-    
 });
- 
 
-
-        
 beginGame();
 
-    
-    document.keyCode = function(event) {
+document.keyCode = function (event) {
 
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log("User guess " + userGuess);
+    console.log("User guess " + userGuess);
 
-        compare(userGuess);
-        rounds ();
-        
-        
-    }
+    compare(userGuess);
+    rounds();
+}
