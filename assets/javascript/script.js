76ff9018b4aca53e25c3fdef248adf555e3ad4ec
rowgregory <rowgregory@gmail.com>
2021-11-17 18:22:48 -0500

    Reset variables at game end

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,214 +1,251 @@
-// Global variables
-// ===========================================
+var letters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var selectableWords =
+  // create an array of words
+  [
+    {
+      word: 'sonic the hedgehog',
+      image: 'assets/images/sonic_giphy.gif',
+    },
+    {
+      word: 'shadow',
+      image: 'assets/images/shadow.gif',
+    },
+    {
+      word: 'knuckles',
+      image: 'assets/images/knuckles_gif.gif',
+    },
+    {
+      word: 'amy',
+      image: 'assets/images/amy_gif.gif',
+    },
+    {
+      word: 'tails miles prower',
+      image: 'assets/images/tails_gif.gif',
+    },
+    {
+      word: 'eggman',
+      image: 'assets/images/eggman_giphy.gif',
+    },
+    {
+      word: 'infinite',
+      image: 'assets/images/infinite_gif.gif',
+    },
+    {
+      word: 'chaos emeralds',
+      image: 'assets/images/chaosEmeralds_gif.gif',
+    },
+    {
+      word: 'sonic generations',
+      image: 'assets/images/sonic_generations_gif.gif',
+    },
+    {
+      word: 'sonic unleashed',
+      image: 'assets/images/sonic_unleashed_gif.gif',
+    },
+    {
+      word: 'metal sonic',
+      image: 'assets/images/metal_sonic_gif.gif',
+    },
+    {
+      word: 'supersonic',
+      image: 'assets/images/superSonic_giphy.gif',
+    },
+    {
+      word: 'rouge the bat',
+      image: 'assets/images/rouge_gif.gif',
+    },
+    {
+      word: 'sonic forces',
+      image: 'assets/images/sonicForces_gif.gif',
+    },
+    {
+      word: 'sonic lost world',
+      image: 'assets/images/sonicLostWorld.gif',
+    },
+  ];
 
-var selectableWords =           // create an array of words
-    [
-        {
-            word: "sonic the hedgehog",
-            image: "assets/images/sonic_giphy.gif",
-        },
-        {
-            word: "shadow",
-            image: "assets/images/shadow.gif",
-        },
-        {
-            word: "knuckles",
-            image: "assets/images/knuckles_gif.gif",
-        },
-        {
-            word: "amy",
-            image: "assets/images/amy_gif.gif",
-        },
-        {
-            word: "tails miles prower",
-            image: "assets/images/tails_gif.gif",
-        },
-        {
-            word: "eggman",
-            image: "assets/images/eggman_giphy.gif",
-        },
-        {
-            word: "infinite",
-            image: "assets/images/infinite_gif.gif",
-        },
-        {
-            word: "chaos emeralds",
-            image: "assets/images/chaosEmeralds_gif.gif",
-        },
-        {
-            word: "sonic generations",
-            image: "assets/images/sonic_generations_gif.gif",
-        },
-        {
-            word: "sonic unleashed",
-            image: "assets/images/sonic_unleashed_gif.gif",
-        },
-        {
-            word: "metal sonic",
-            image: "assets/images/metal_sonic_gif.gif",
+var wins = 0;
+var losses = 0;
+var guessesLeft = 12;
+var currentWord = '';
+var currentImage = '';
+var videoGameLetters = [];
+var numBlanks = 0;
+var wrongLtrs = [];
+var answer = [];
+var guessedLetter = '';
+var letterBtn;
 
-        },
-        {
-            word: "supersonic",
-            image: "assets/images/superSonic_giphy.gif",
-        },
-        {
-            word: "rouge the bat",
-            image: "assets/images/rouge_gif.gif",
-        },
-        {
-            word: "sonic forces",
-            image: "assets/images/sonicForces_gif.gif",
-        },
-        {
-            word: "sonic lost world",
-            image: "assets/images/sonicLostWorld.gif",
-        },
+const underscores = document.getElementById('underscore');
+const guessesRemaining = document.getElementById('remaining');
+const totalWins = document.getElementById('wins');
+const totalLosses = document.getElementById('losses');
+const incorrectGuesses = document.getElementById('guessed');
+const winSound = document.getElementById('cash');
+const winImg = document.getElementById('winImage');
+const loseSound = document.getElementById('shield');
+const loseImg = document.getElementById('mad');
 
-    ];
+$(() => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
-var wins = 0;                   // how many wins the player has racked up
-var losses = 0;                 // tallies number of lossses
-var guessesLeft = 12;           // guesses the user has left
+const createKeyboard = () => {
+  for (i = 0; i < letters.length; i++) {
+    letterBtn = $('<button>');
 
-var currentWord = "";
-var currentImage = "";          // left empty to store word
-var videoGameLetters = [];      // empty array to store letters of currentWord
-var numBlanks = 0;              // holds the number of "blanks"(where letters will pop up)
-var wrongLtrs = [];             // empty array to store incorrect guesses
-var answer = [];                // empty array to store users answer
-var letterClicked = '';
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-
-for (i = 0; i < letters.length; i++) {
-
-    var letterBtn = $("<button>");
-
-    letterBtn.addClass("letter-button letter letter-button-color");
-    letterBtn.attr("data-letter", letters[i]);
+    letterBtn.addClass('letter-button letter letter-button-color');
+    letterBtn.attr('id', letters[i]);
     letterBtn.text(letters[i]);
-    $("#buttons").append(letterBtn);
+    $('#buttons').append(letterBtn);
+  }
+
+  $('.letter-button').on('click', (e) => {
+    guessedLetter = $(e.target).text().toLowerCase();
+    determineCorrectLetter(guessedLetter.toLowerCase());
+  });
 };
 
-$(".letter-button").on("click", function () {
-    letterClicked = $(event.target).text().toLowerCase();
-    letterButtonClicked(letterClicked);
+const displayStats = () => {
+  underscores.innerHTML = answer.join('');
+  guessesRemaining.innerHTML = `Guesses remaing: ${guessesLeft}`;
+  totalWins.innerHTML = `Wins: ${wins}`;
+  totalLosses.innerHTML = `Losses: ${losses}`;
+  incorrectGuesses.innerHTML = `Incorrect letters: ${wrongLtrs}`;
+};
 
-    var fridgeMagnet = $("<div>");
+const getWordToGuess = (cb) => {
+  var accessedIndex = Math.floor(Math.random() * selectableWords.length);
+  currentWord = selectableWords[accessedIndex].word;
+  currentImage = selectableWords[accessedIndex].image;
 
-    fridgeMagnet.addClass("letter fridge-color");
-    fridgeMagnet.text($(this).attr("data-letter"));
-    $("answer").append(fridgeMagnet);
-});
+  videoGameLetters = currentWord.split('');
 
-// DOM manipulation 
-// =============================================
+  for (var i = 0, j = videoGameLetters.length; i < j; i++) {
+    if (videoGameLetters[i] === ' ') {
+      answer.push(' ');
+    } else {
+      answer.push('_');
+    }
+  }
 
-window.onload = function () {
-    document.getElementById("underscore").innerHTML = answer.join("");
-    document.getElementById("remaining").innerHTML = "Guesses remaing: " + guessesLeft;
-    document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
-    document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
-    document.getElementById("guessed").innerHTML = "Choosen letters: " + " " + wrongLtrs;
-}
+  return cb();
+};
 
-function beginGame() {
+const determineCorrectLetter = (x) => {
+  for (var i = 0; i < videoGameLetters.length; i++) {
+    if (x === videoGameLetters[i]) {
+      answer[i] = x;
+      document.getElementById('ring').play();
+    }
+  }
 
-    $("#reset").hide();
-    // === uses Math floor to select word at random
-    var accessedIndex = Math.floor(Math.random() * selectableWords.length);
-    currentWord = selectableWords[accessedIndex].word;
-    currentImage = selectableWords[accessedIndex].image;
+  const wrongLetter = !(videoGameLetters.indexOf(x) > -1);
 
-    // === divide (split) randomWord into an individual letters and store it into videoGameletters
-    videoGameLetters = currentWord.split("");
+  if (wrongLtrs.includes(x)) {
+    console.log('here');
+    document.getElementById('killed').play();
+  } else if (wrongLetter) {
+    $(`#${x.toUpperCase()}`).addClass('darken');
 
-    guessesLeft = 12;
+    wrongLtrs.push(x);
+    guessesLeft--;
+    document.getElementById('killed').play();
+  }
+  updateScore();
+};
+
+const updateScore = () => {
+  guessesRemaining.innerHTML = `Guesses remaining: ${guessesLeft}`;
+  incorrectGuesses.innerHTML = `Incorrect letters: ${wrongLtrs.join(' ')}`;
+  underscores.innerHTML = answer.join('');
+
+  const userWon = videoGameLetters.toString() == answer.toString();
+
+  if (userWon) {
+    win();
+  } else if (guessesLeft == 0) {
+    lose();
+  }
+};
+
+const win = () => {
+  wins++;
+  winSound.play();
+  winImg.src = currentImage;
+  winImg.style.visibility = 'visible';
+  totalWins.innerHTML = `Wins: ${wins}`;
+  $('#myModal').modal('show');
+  $('.modal-title').text('You won!');
+};
+
+const lose = () => {
+  losses++;
+  loseSound.play();
+  loseImg.style.visibility = 'visible';
+  totalLosses.innerHTML = `'Losses: ${losses}`;
+  $('#myModal').modal('show');
+  $('.modal-title').text('You lost');
+};
+
+$('#reset').on('click', () => {
+  const reset = () => {
+    currentWord = '';
+    currentImage = '';
+    videoGameLetters = [];
+    numBlanks = 0;
     wrongLtrs = [];
     answer = [];
+    guessedLetter = '';
+    guessesLeft = 12;
 
-    for (var i=0, j=videoGameLetters.length; i < j; i++){
-        // Put a space instead of an underscore between multi word "words"
-        if (videoGameLetters[i] === " ") {
-            answer.push(" ")
-        } else {
-            answer.push("_")
-        }
-    }
-}
+    underscores.innerHTML = answer.join('');
+    guessesRemaining.innerHTML = `Guesses remaing: ${guessesLeft}`;
+    totalWins.innerHTML = `Wins: ${wins}`;
+    totalLosses.innerHTML = `Losses: ${losses}`;
+    incorrectGuesses.innerHTML = `Choosen letters: ${wrongLtrs}`;
 
-function letterButtonClicked(x) {
+    $('button').each(() => {
+      $('button').removeClass('darken');
+    });
 
-    x = x.toLowerCase();
-    // === determine if key choosen was in the alphabet
-    for (var i = 0; i < videoGameLetters.length; i++) { 		//scans through every letter in the array
-        if (x === videoGameLetters[i]) {  				//if the letter that was clicked equals the letters of the random word
-            answer[i] = x;
-            document.getElementById("ring").play();				//then replace the blank space with the clicked letter
-        }
-    }
-    $('#underscore').text(answer.join(' '));		//adds answer from letter arrary and displays it
+    return getWordToGuess(displayStats);
+  };
 
-    if (!(videoGameLetters.indexOf(x) > -1)) {			//if the word does not have the letter 
-        document.getElementById("killed").play();
-        wrongLtrs.push(x);
-        guessesLeft--;
-    }
-    rounds();
-    console.log("This is the answer " + answer);
-}
-
-function rounds() {
-
-    document.getElementById("mad").style.visibility = "hidden";
-    document.getElementById("winImage").style.visibility = "hidden";
-    document.getElementById("remaining").innerHTML = "Guesses remaining: " + " " + guessesLeft;
-    document.getElementById("underscore").innerHTML = answer.join("");
-    document.getElementById("guessed").innerHTML = "Already used: " + " " + wrongLtrs.join(" ");
-
-    if (videoGameLetters.toString() == answer.toString()) {
-        wins++;
-
-        //update wins
-        document.getElementById("winImage").src = currentImage
-        document.getElementById("cash").play();
-        document.getElementById("wins").innerHTML = "Wins: " + " " + wins;
-        document.getElementById("winImage").style.visibility = "visible";
-        alert("You won");
-        $("#reset").show();
-        $('#myModal').modal('show');  
-
-    } else if (guessesLeft == 0) {
-        losses++;
-        document.getElementById("shield").play();
-        document.getElementById("mad").style.visibility = "visible";
-        alert("you lose!");
-        $("#reset").show();
-        $('#myModal').modal('show');
-
-        // Update the losses in the HTML document
-        document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
-        document.getElementById("guessed").innerHTML = "Choosen letters:" + " " + wrongLtrs.join(" ");
-    }
-}
-
-$('#reset').on('click', function () {
-
-    beginGame();
+  reset();
 });
 
-beginGame();
+const start = () => {
+  createKeyboard();
+  getWordToGuess(displayStats);
+};
 
-document.keyCode = function (event) {
-
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("User guess " + userGuess);
-
-    compare(userGuess);
-    rounds();
-}
+start();
